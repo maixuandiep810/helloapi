f4e5266fc3e8621c1f693df6fe8e206aec0874d9
maixuandiep810 <maixuandiep810@gmail.com>
2020-12-01 23:34:43 +0700

    add hanoi

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -35,5 +35,17 @@ namespace helloapi.Controllers
             })
             .ToArray();
         }
+        [HttpGet("hanoi")]
+        public IEnumerable<WeatherForecast> GetHanoi()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
